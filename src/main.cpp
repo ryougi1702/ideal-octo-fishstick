686b9842bdd6865fcd88968425d2a8bbdb52acab
Poh San <wangpohshan@gmail.com>
2025-03-17 07:54:33 +0800

    finally got a window to open

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
+#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    // Create the main window
+    // sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
+    sf::Window window(sf::VideoMode({800, 600}), "SFML window");
 
+    // run the program as long as the window is open
     while (window.isOpen())
     {
+        // check all the window's events that were triggered since the last iteration of the loop
         while (const std::optional event = window.pollEvent())
         {
+            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
-
-        window.clear();
-        window.display();
     }
 }
